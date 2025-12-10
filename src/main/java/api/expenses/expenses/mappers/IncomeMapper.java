19a554d9be790075e6323890fe93e2767dde2f2a
mati <mferrerovilas@frba.utn.edu.ar>
2025-12-10 12:26:21 -0300

    Arreglo Banco Enum

--- src/main/java/api/expenses/expenses/mappers/IncomeMapper.java
+++ src/main/java/api/expenses/expenses/mappers/IncomeMapper.java
@@ -1,10 +1,12 @@
 package api.expenses.expenses.mappers;
 
 import api.expenses.expenses.entities.Income;
+import api.expenses.expenses.enums.BanksEnum;
 import api.expenses.expenses.records.income.IncomeRecord;
 import api.expenses.expenses.records.income.IncomeToAdd;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -14,10 +16,18 @@ import java.util.List;
 public interface IncomeMapper {
     @Mapping(target = "currency", ignore = true)
     @Mapping(target = "userGroups", ignore = true)
-    Income toEntity(IncomeToAdd incomeToAdd);
+    @Mapping(
+            target = "bank",
+            source = "bank",
+            qualifiedByName = "bankFromDescription"
+    )    Income toEntity(IncomeToAdd incomeToAdd);
 
     @Mapping(source = "userGroups", target = "groups")
     IncomeRecord toRecord(Income income);
     List<IncomeRecord> toRecord(List<Income> income);
 
+    @Named("bankFromDescription")
+    static BanksEnum mapBank(String bank) {
+        return BanksEnum.findByDescription(bank);
+    }
 }
