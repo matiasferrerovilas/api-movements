93874fcdaec07d415d429eccb06c84e674ed05f4
mferrero1 <mferrero@max.capital>
2026-02-17 22:03:14 +0100

    Arreglo sscript

--- src/main/resources/db/changelog/018_alter_table_services_add_indexs.sql
+++ src/main/resources/db/changelog/018_alter_table_services_add_indexs.sql
@@ -1,6 +1,8 @@
 -- liquibase formatted sql
+-- changeset mati:018-add-unique-services
+
 ALTER TABLE services
-    DROP INDEX uc_services_user_desc;
+    DROP INDEX uc_services_description;
 
 ALTER TABLE services
     ADD CONSTRAINT uc_services_user_currency_desc UNIQUE (user_id, currency_id, description);
