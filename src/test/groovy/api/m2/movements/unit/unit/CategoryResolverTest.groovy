f5aca0326c878dc4dd4bacc6c135bfe1f58cf92a
mferrero1 <mferrero@max.capital>
2026-03-08 17:42:54 +0100

    test: ✅ Spock spec for api.m2.movements.services.category.CategoryResolver

---
+++ src/test/groovy/api/m2/movements/unit/unit/CategoryResolverTest.groovy
@@ -0,0 +1,61 @@
+package api.m2.movements.unit.unit
+
+import api.m2.movements.entities.Category
+import api.m2.movements.records.categories.CategoryRecord
+import api.m2.movements.services.category.CategoryAddService
+import api.m2.movements.services.category.CategoryResolver
+import spock.lang.Specification
+import spock.lang.Subject
+
+class CategoryResolverTest extends Specification {
+
+    CategoryAddService categoryAddService = Mock(CategoryAddService)
+
+    @Subject
+    CategoryResolver service
+
+    def setup() {
+        service = new CategoryResolver(categoryAddService)
+    }
+
+    def "resolve - should add category when record is null"() {
+        given:
+        def defaultCategory = "Default Category"
+        categoryAddService.getDefaultCategory() >> defaultCategory
+        def category = Stub(Category) { getId() >> 1L }
+
+        when:
+        def result = service.resolve(null as CategoryRecord)
+
+        then:
+        1 * categoryAddService.addCategory(defaultCategory) >> category
+        result == category
+    }
+
+    def "resolve - should add category when record is not null"() {
+        given:
+        def description = "Test Category"
+        def record = new CategoryRecord(description)
+        def category = Stub(Category) { getId() >> 1L }
+
+        when:
+        def result = service.resolve(record)
+
+        then:
+        1 * categoryAddService.addCategory(description) >> category
+        result == category
+    }
+
+    def "resolve - should add category when description is provided"() {
+        given:
+        def description = "Test Category"
+        def category = Stub(Category) { getId() >> 1L }
+
+        when:
+        def result = service.resolve(description)
+
+        then:
+        1 * categoryAddService.addCategory(description) >> category
+        result == category
+    }
+}
